02fecca0b075c42f7a30c0817a5d396c07c36b32
DimiConstantin <168590061+DimiConstantin@users.noreply.github.com>
2025-05-20 01:12:13 +0300

    Create paths.h

---
+++ paths.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define LOGIN_PATH "/api/v1/tema/admin/login"
+#define ADD_USER_PATH "/api/v1/tema/admin/users"
+#define GET_USERS_PATH "/api/v1/tema/admin/users"
+#define DELETE_USER_PATH "/api/v1/tema/admin/users/"
+#define USER_LOGIN_PATH "/api/v1/tema/user/login"
+
