ab62fb9fd547a5881754b091b0c4b78d8b8b841c
DimiConstantin <168590061+DimiConstantin@users.noreply.github.com>
2025-05-20 16:02:51 +0300

    Update paths.h

--- paths.h
+++ paths.h
@@ -5,4 +5,8 @@
 #define GET_USERS_PATH "/api/v1/tema/admin/users"
 #define DELETE_USER_PATH "/api/v1/tema/admin/users/"
 #define USER_LOGIN_PATH "/api/v1/tema/user/login"
-
+#define LOGOUT_ADMIN "/api/v1/tema/admin/logout"
+#define GET_ACCESS_PATH "/api/v1/tema/library/access"
+#define USER_LOGOUT_PATH "/api/v1/tema/user/logout"
+#define GET_MOVIES_PATH "/api/v1/tema/library/movies"
+#define GET_MOVIE_PATH "/api/v1/tema/library/movies/"
