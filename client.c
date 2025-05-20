a673dfbb8bc2fdaa4260cee6ecee7a475006bbd8
DimiConstantin <168590061+DimiConstantin@users.noreply.github.com>
2025-05-20 16:02:25 +0300

    movies

--- client.c
+++ client.c
@@ -19,6 +19,11 @@ char *session_cookie = NULL;
 char *JWT_cookie = NULL;
 
 void handle_login_admin(int server_fd) {
+    if (session_cookie != NULL) {
+        printf("ERROR: An user is already logged in\n");
+        return;
+    }
+
     char username[INPUT_LEN];
     char password[INPUT_LEN];
 
@@ -41,13 +46,24 @@ void handle_login_admin(int server_fd) {
     char *content_type = "application/json";
     char *body_parts[1];
     body_parts[0] = json_payload;
-    char *message = compute_post_request(SERVER_IP, LOGIN_PATH, content_type, body_parts, 1, NULL, 0);
+    char *message = compute_post_request(SERVER_IP, LOGIN_PATH, content_type, body_parts, 1, NULL, 0, NULL, 0);
 
     // send request to server
     send_to_server(server_fd, message);
 
     char *response = receive_from_server(server_fd);
     // check the response and extract the session cookie
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+
+    if (status_code != 200) {
+        printf("ERROR: Admin login failed\n");
+        free(message);
+        json_free_serialized_string(json_payload);
+        json_value_free(root_value);
+        return;
+    }
+
     char *cookie_hdr = strstr(response, "Set-Cookie: ");
     if (cookie_hdr) {
         char *cookie_start = cookie_hdr + strlen("Set-Cookie: ");
@@ -58,13 +74,10 @@ void handle_login_admin(int server_fd) {
             session_cookie = malloc(cookie_len + 1);
             strncpy(session_cookie, cookie_start, cookie_len);
             session_cookie[cookie_len] = '\0';
-
-            printf("SUCCESS: Admin logged in\n");
-        }
-        else {
-            printf("ERROR: Admin login failed\n");
         }
     }
+
+    printf("SUCCESS: Admin logged in\n");
 
     free(message);
     json_free_serialized_string(json_payload);
@@ -99,7 +112,7 @@ void handle_add_user(int server_fd) {
     char *content_type = "application/json";
     char *body_parts[1];
     body_parts[0] = json_payload;
-    char *message = compute_post_request(SERVER_IP, ADD_USER_PATH, content_type, body_parts, 1, &session_cookie, 1);
+    char *message = compute_post_request(SERVER_IP, ADD_USER_PATH, content_type, body_parts, 1, &session_cookie, 1, NULL, 0);
 
     send_to_server(server_fd, message);
 
@@ -131,7 +144,7 @@ void handle_get_users(int server_fd) {
 
     char *cookies[1] = {session_cookie};
 
-    char *message = compute_get_request(SERVER_IP, GET_USERS_PATH, NULL, cookies, 1);
+    char *message = compute_get_request(SERVER_IP, GET_USERS_PATH, NULL, cookies, 1, NULL, 0);
     send_to_server(server_fd, message);
 
     char *response = receive_from_server(server_fd);
@@ -199,25 +212,35 @@ void handle_delete_user(int server_fd) {
     }
 }
 
-void handle_logout_admin() {
+void handle_logout_admin(int server_fd) {
     if (session_cookie == NULL) {
         printf("ERROR: Admin not logged in\n");
         return;
     }
 
     // create GET request
+    char *cookies[1] = {session_cookie};
+    char *message = compute_get_request(SERVER_IP, LOGOUT_ADMIN, NULL, cookies, 1, NULL, 0);
+    send_to_server(server_fd, message);
+
+    char *response = receive_from_server(server_fd);
+
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+
+    if (status_code != 200) {
+        printf("ERROR: Failed to logout admin\n");
+        free(message);
+        return;
+    }
 
     free(session_cookie);
+    free(message);
     session_cookie = NULL;
     printf("SUCCESS: Admin logged out\n");
 }
 
 void handle_login(int server_fd) {
-    if (session_cookie == NULL) {
-        printf("ERROR: Admin not logged in\n");
-        return;
-    }
-
     char admin_username[INPUT_LEN];
     char password[INPUT_LEN];
     char username[INPUT_LEN];
@@ -245,9 +268,114 @@ void handle_login(int server_fd) {
     // create POST request
     char *content_type = "application/json";
     char *body_parts[1] = {json_payload};
-    char *cookies[1] = {session_cookie};
 
-    char *message = compute_post_request(SERVER_IP, USER_LOGIN_PATH, content_type, body_parts, 1, cookies, 1);
+    char *message = compute_post_request(SERVER_IP, USER_LOGIN_PATH, content_type, body_parts, 1, NULL, 0, NULL, 0);
+    send_to_server(server_fd, message);
+
+    char *response = receive_from_server(server_fd);
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+
+    if (status_code == 200) {
+        printf("SUCCESS: User logged in\n");
+
+        char *cookie_hdr = strstr(response, "Set-Cookie: ");
+        if (cookie_hdr) {
+            char *cookie_start = cookie_hdr + strlen("Set-Cookie: ");
+            char *cookie_end = strstr(cookie_start, ";");
+            if (cookie_end) {
+                size_t cookie_len = cookie_end - cookie_start;
+                free(session_cookie);
+                session_cookie = malloc(cookie_len + 1);
+                strncpy(session_cookie, cookie_start, cookie_len);
+                session_cookie[cookie_len] = '\0';
+            }
+        }
+    }
+    else if (status_code == 403) {
+        printf("ERROR: Invalid credentials\n");
+    }
+    else {
+        printf("ERROR: Failed to login\n");
+    }
+}
+
+void handle_get_access(int server_fd) {
+    if (session_cookie == NULL) {
+        printf("ERROR: User not logged in\n");
+        return;
+    }
+
+    char *cookies[1] = {session_cookie};
+    char *message = compute_get_request(SERVER_IP, GET_ACCESS_PATH, NULL, cookies, 1, NULL, 0);
+
+    send_to_server(server_fd, message);
+
+    char *response = receive_from_server(server_fd);
+
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+    if (status_code != 200) {
+        printf("ERROR: Failed to get access\n");
+        free(message);
+        return;
+    }
+
+    char *token_hdr = basic_extract_json_response(response);
+    JSON_Value *root_value = json_parse_string(token_hdr);
+    JSON_Object *root_object = json_value_get_object(root_value);
+    const char *token = json_object_get_string(root_object, "token");
+    
+    JWT_cookie = malloc(strlen(token) + 1);
+    strcpy(JWT_cookie, token);
+
+    printf("SUCCESS: Access granted\n");
+}
+
+void handle_logout(int server_fd) {
+    if (session_cookie == NULL) {
+        printf("ERROR: User not logged in\n");
+        return;
+    }
+
+    char *cookies[1] = {session_cookie};
+    char *message = compute_get_request(SERVER_IP, USER_LOGOUT_PATH, NULL, cookies, 1, NULL, 0);
+    send_to_server(server_fd, message);
+
+    char *response = receive_from_server(server_fd);
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+
+    if (status_code != 200) {
+        printf("ERROR: Failed to logout\n");
+        free(message);
+        return;
+    }
+
+    free(session_cookie);
+    free(JWT_cookie);
+
+    session_cookie = NULL;
+    JWT_cookie = NULL;
+
+    printf("SUCCESS: User logged out\n");
+}
+
+void handle_get_movies(int server_fd) {
+    if (session_cookie == NULL) {
+        printf("ERROR: User not logged in\n");
+        return;
+    }
+
+    if (JWT_cookie == NULL) {
+        printf("ERROR: User access is not granted\n");
+        return;
+    }
+
+    char *jwt_header = build_jwt_header(JWT_cookie);
+    char *headers[1] = {jwt_header};
+
+    char *message = compute_get_request(SERVER_IP, GET_MOVIES_PATH, NULL, NULL, 0, headers, 1);
     send_to_server(server_fd, message);
 
     char *response = receive_from_server(server_fd);
@@ -255,10 +383,160 @@ void handle_login(int server_fd) {
     int status_code = 0;
     sscanf(response, "HTTP/1.1 %d", &status_code);
 
-    printf("Response: \n%s\n", response);
-    if (status_code == 200) {
-        printf("SUCCESS: User logged in\n");
-        printf("Response: \n%s\n", response);
+    if (status_code != 200) {
+        printf("ERROR: Failed to get movies\n");
+        free(message);
+        return;
+    }
+
+    char *json_response = basic_extract_json_response(response);
+    JSON_Value *root_value = json_parse_string(json_response);
+    JSON_Object *root_object = json_value_get_object(root_value);
+    JSON_Array *movies_array = json_object_get_array(root_object, "movies");
+    size_t movie_count = json_array_get_count(movies_array);
+
+    printf("SUCCESS: Movies:\n");
+    for (size_t i = 0; i < movie_count; i++) {
+        JSON_Object *movie_object = json_array_get_object(movies_array, i);
+        const char *title = json_object_get_string(movie_object, "title");
+        const int id = json_object_get_number(movie_object, "id");
+        printf("#%d %s\n", id, title);
+    }
+
+    json_value_free(root_value);
+    free(message);
+}
+
+void handle_get_movie(int server_fd) {
+    if (session_cookie == NULL) {
+        printf("ERROR: User not logged in\n");
+        return;
+    }
+
+    if (JWT_cookie == NULL) {
+        printf("ERROR: User access is not granted\n");
+        return;
+    }
+
+    char id[INPUT_LEN];
+    printf("id=");
+    fgets(id, INPUT_LEN, stdin);
+    remove_trailing_newline(id);
+
+    // create GET request
+    int len = strlen(GET_MOVIE_PATH) + strlen(id) + 1;
+    char *url = malloc(len);
+    snprintf(url, len, "%s%s", GET_MOVIE_PATH, id);
+
+    char *jwt_header = build_jwt_header(JWT_cookie);
+    char *headers[1] = {jwt_header};
+    char *message = compute_get_request(SERVER_IP, url, NULL, NULL, 0, headers, 1);
+    send_to_server(server_fd, message);
+
+    char *response = receive_from_server(server_fd);
+
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+
+    if (status_code == 404) {
+        printf("ERROR: Movie not found\n");
+        free(message);
+        free(url);
+        return;
+    }
+    else if (status_code != 200) {
+        printf("ERROR: Failed to get movie\n");
+        free(message);
+        free(url);
+        return;
+    }
+
+    printf("SUCCESS: Movie details:\n");
+
+    char *json_response = basic_extract_json_response(response);
+    JSON_Value *root_value = json_parse_string(json_response);
+    JSON_Object *root_object = json_value_get_object(root_value);
+    const char *title = json_object_get_string(root_object, "title");
+    const int year = json_object_get_number(root_object, "year");
+    const char *description = json_object_get_string(root_object, "description");
+    const char *rating = json_object_get_string(root_object, "rating");
+
+    printf("title: %s\n", title);
+    printf("year: %d\n", year);
+    printf("description: %s\n", description);
+    printf("rating: %.1f\n", atof(rating));
+
+    json_value_free(root_value);
+    free(message);
+    free(url);
+    free(response);
+
+    return;
+}
+
+void handle_add_movie(int server_fd) {
+    if (session_cookie == NULL) {
+        printf("ERROR: User not logged in\n");
+        return;
+    }
+
+    if (JWT_cookie == NULL) {
+        printf("ERROR: User access is not granted\n");
+        return;
+    }
+
+    char title[INPUT_LEN];
+    char year[INPUT_LEN];
+    char description[INPUT_LEN];
+    char rating[INPUT_LEN];
+
+    // read movie details from stdin
+    printf("title=");
+    fgets(title, INPUT_LEN, stdin);
+    remove_trailing_newline(title);
+    printf("year=");
+    fgets(year, INPUT_LEN, stdin);
+    remove_trailing_newline(year);
+    printf("description=");
+    fgets(description, INPUT_LEN, stdin);
+    remove_trailing_newline(description);
+    printf("rating=");
+    fgets(rating, INPUT_LEN, stdin);
+    remove_trailing_newline(rating);
+
+    // create JSON object for adding movie
+    JSON_Value *root_value = json_value_init_object();
+    JSON_Object *root_object = json_value_get_object(root_value);
+    json_object_set_string(root_object, "title", title);
+    json_object_set_number(root_object, "year", atoi(year));
+    json_object_set_string(root_object, "description", description);
+    json_object_set_number(root_object, "rating", atof(rating));
+    
+    char *json_payload = json_serialize_to_string(root_value);
+
+    // create POST request
+    char *content_type = "application/json";
+    char *body_parts[1];
+    body_parts[0] = json_payload;
+    
+    char *jwt_header = build_jwt_header(JWT_cookie);
+    char *headers[1] = {jwt_header};
+
+    char *message = compute_post_request(SERVER_IP, GET_MOVIES_PATH, content_type, body_parts, 1, NULL, 0, headers, 1);
+
+    send_to_server(server_fd, message);
+
+    char *response = receive_from_server(server_fd);
+
+    int status_code = 0;
+    sscanf(response, "HTTP/1.1 %d", &status_code);
+
+    if (status_code == 201) {
+        printf("SUCCESS: Movie added\n");
+        free(message);
+        free(json_payload);
+        json_value_free(root_value);
+        return;
     }
 }
 
@@ -290,10 +568,25 @@ int main() {
             handle_delete_user(server_fd);
         }
         else if (strcmp(command, "logout_admin") == 0) {
-            handle_logout_admin();
+            handle_logout_admin(server_fd);
         }
         else if (strcmp(command, "login") == 0) {
             handle_login(server_fd);
+        }
+        else if (strcmp(command, "get_access") == 0) {
+            handle_get_access(server_fd);
+        }
+        else if (strcmp(command, "logout") == 0) {
+            handle_logout(server_fd);
+        }
+        else if (strcmp(command, "get_movies") == 0) {
+            handle_get_movies(server_fd);
+        }
+        else if (strcmp(command, "get_movie") == 0) {
+            handle_get_movie(server_fd);
+        }
+        else if (strcmp(command, "add_movie") == 0) {
+            handle_add_movie(server_fd);
         }
         else {
             printf("ERROR: Unknown command\n");
